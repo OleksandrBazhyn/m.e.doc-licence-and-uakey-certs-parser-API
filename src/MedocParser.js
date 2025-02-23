e216e3c59c83b230234dd30568a4e9edce50018f
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-02-23 16:49:36 +0200

    Created BaseParser class for parsers

--- src/MedocParser.js
+++ src/MedocParser.js
@@ -1,43 +1,8 @@
-import { Builder, Browser, By, until, Key, chrome } from "selenium-webdriver";
+import { By, until, Key } from "selenium-webdriver";
+import BaseParser from "./BaseParser.js";
 import Debuger from "./Debuger.js";
 
-class MedocParser {
-    constructor(debugMode = false) {
-        this.driver = null;
-        this.debugMode = debugMode;
-    }
-
-    async init() {
-            this.log("Initializing WebDriver...");
-            const options = new chrome.Options();
-            if (!this.debugMode) {
-                options.addArguments("--headless", "--window-size=1920,1080");
-            }
-            this.driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
-    }
-
-    async dispose() {
-        this.log("Disposing WebDriver...");
-        if (this.driver) {
-            await this.driver.quit();
-            this.driver = null;
-        }
-    }
-
-    log(message) {
-        if (this.debugMode) console.log(`MedocParser: ${message}`);
-    }
-
-    async waitForElement(selector, timeout = 5000) {
-        return await this.driver.wait(until.elementLocated(By.css(selector)), timeout);
-    }
-
-    async navigateTo(url) {
-        this.log(`Navigating to ${url}`);
-        await this.driver.get(url);
-        await this.driver.wait(until.elementLocated(By.css("body")), 1000);
-    }
-
+class MedocParser extends BaseParser {
     async searchUSREOU(USREOU) {
         const recapcha = await this.waitForElement(".recaptcha-checkbox-border");
         await recapcha.click();
@@ -48,7 +13,7 @@ class MedocParser {
         await inputField.sendKeys(USREOU, Key.RETURN);
         await this.driver.sleep(10000);
 
-        await this.driver.wait(until.elementLocated(By.css(".popupRes")), 5000)
+        await this.driver.wait(until.elementLocated(By.css(".popupRes")), 5000);
     }
 
     async getFullInfo(USREOU) {
@@ -58,7 +23,6 @@ class MedocParser {
         try {
             await this.navigateTo("https://medoc.ua/getcode");
             await this.searchUSREOU(USREOU);
-            
         } catch (err) {
             console.error("[ERROR] Exception in getFullInfo:", err);
             const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
--- src/app.js
+++ src/app.js
@@ -5,21 +5,20 @@ import fs from "node:fs";
 (async () => {
     const USREOU = 2804120785;
     const debugMode = true;
-    // const UKparser = new UakeyParser(debugMode);
+
+    const uakeyParser = new UakeyParser(debugMode);
     const medocParser = new MedocParser(debugMode);
     
     try {
-        // await UKparser.init();
-
-        // const certs = await UKparser.getFullInfo(USREOU);
+        await uakeyParser.init();
+        const uakeyCerts = await uakeyParser.getFullInfo(USREOU);
         
-        // if (certs) {
-        //     fs.writeFileSync("certificates.json", JSON.stringify(certs, null, 2), "utf-8");
-        //     console.log("Certificates saved successfully.");
-        // } else {
-        //     console.warn("No certificates found or an error occurred.");
-        // }
-
+        if (uakeyCerts) {
+            fs.writeFileSync("certificates.json", JSON.stringify(uakeyCerts, null, 2), "utf-8");
+            console.log("Uakey certificates saved successfully.");
+        } else {
+            console.warn("No Uakey certificates found or an error occurred.");
+        }
 
         await medocParser.init();
         await medocParser.getFullInfo(USREOU);
@@ -27,6 +26,7 @@ import fs from "node:fs";
     } catch (err) {
         console.error("[FATAL ERROR]", err);
     } finally {
+        await uakeyParser.dispose();
         await medocParser.dispose();
     }
 })();
