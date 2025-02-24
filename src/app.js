3ca58217af44a01bf513e2cbff671ecdc47799ee
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-02-24 16:15:03 +0200

    This function retrieves certificate data, checks their expiration dates

--- src/app.js
+++ src/app.js
@@ -2,6 +2,33 @@ import UakeyParser from "./UakeyParser.js";
 import MedocParser from "./MedocParser.js";
 import fs from "node:fs";
 
+async function findCertificatesExpire(USREOUList) {
+    try {
+        const parser = new UakeyParser();
+        await parser.init();
+
+        const uakeyCerts = await parser.getFullInfo(USREOUList);
+
+        let codes = new Set();
+        const date = /\b03\.2025\b/;
+
+        for (let comp of uakeyCerts.results) {
+            for (let cert of comp.data) {
+                if (!date.test(cert.endDate)) {
+                    continue;
+                }
+                codes.add(cert.code);
+            }
+        }
+
+        await parser.dispose();
+        return Array.from(codes);
+    } catch (err) {
+        console.error("Error in method findCertificatesExpire:", err);
+        return [];
+    }
+}
+
 (async () => {
     const USREOU = [2804120785, 2424];
     const debugMode = true;
@@ -12,17 +39,21 @@ import fs from "node:fs";
     try {
         await uakeyParser.init();
         await medocParser.init();
-        //await medocParser.getFullInfo(USREOU); //
         
-        let orgs = await fs.readFileSync("orgs.txt", "utf-8");
-        orgs = orgs.split("\r\n");
-        const uakeyCerts = await uakeyParser.getFullInfo(orgs);
-        fs.writeFileSync("certificates.json", JSON.stringify(uakeyCerts, null, 2), "utf-8");
+        // let orgs = await fs.promises.readFile("orgs.txt", "utf-8");
+        // orgs = orgs.split(/\r?\n/).map(line => line.trim()).filter(line => line.length > 0);
+
+        // const certsExpire = await findCertificatesExpire(orgs);
+        const uakeyCerts = await uakeyParser.getFullInfo(USREOU);
+
+        // await fs.promises.writeFile("certificatesTERM.json", JSON.stringify(certsExpire, null, 2), "utf-8");
+        await fs.promises.writeFile("certificates.json", JSON.stringify(uakeyCerts, null, 2), "utf-8");
+
         console.log("Uakey certificates saved successfully.");
     } catch (err) {
         console.error("[FATAL ERROR]", err);
     } finally {
-        await uakeyParser.dispose();
-        await medocParser.dispose();
+        if (uakeyParser) await uakeyParser.dispose();
+        if (medocParser) await medocParser.dispose();
     }
 })();
